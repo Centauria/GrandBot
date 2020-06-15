93eb7834dc77bf9c4ebad5670447bc73036c500e
Centauria <centauriavc@gmail.com>
2020-06-16 00:38:55 +0800

    update main

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 # -*- coding: utf-8 -*-
-from iotbot import IOTBOT, GroupMsg, FriendMsg, Action
+from iotbot import IOTBOT, Action, GroupMsg, FriendMsg
+import logging
 
 from util import configuration
 
-bot = IOTBOT(configuration.qq, use_plugins=True)
+logging.basicConfig(level=logging.ERROR)
+
+bot = IOTBOT(configuration.qq, use_plugins=True, plugin_dir='plugins')
 action = Action(bot)
 
 
+@bot.on_group_msg
+def on_group_msg(ctx: GroupMsg):
+    if ctx.FromUserId in configuration.admin and ctx.MsgType == 'TextMsg' and ctx.Content[0] == '.':
+        content = ctx.Content.lstrip('.')
+        if content == 'refresh':
+            bot.refresh_plugins()
+            action.send_group_text_msg(ctx.FromGroupId, '插件已刷新')
+        elif content == 'test':
+            action.send_group_pic_msg(ctx.FromGroupId, 'https://t.cn/A6Am7xYO')
+        else:
+            action.send_group_text_msg(ctx.FromGroupId, '不要乱发指令啊喂')
+
+
 @bot.on_friend_msg
-def friend(ctx: FriendMsg):
-    print(f"""内容为：{ctx.Content}""")
-    print(ctx.get('CurrentQQ'))
-    action.send_friend_text_msg(ctx.FromUin, ctx.Content)
+def on_friend_msg(ctx: FriendMsg):
+    if ctx.FromUin in configuration.admin and ctx.MsgType == 'TextMsg' and ctx.Content[0] == '.':
+        content = ctx.Content.lstrip('.')
+        if content == 'refresh':
+            bot.refresh_plugins()
+            action.send_friend_text_msg(ctx.FromUin, '插件已刷新')
+        elif content == 'test':
+            action.send_friend_pic_msg(ctx.FromUin, 'https://t.cn/A6Am7xYO')
+        else:
+            action.send_friend_text_msg(ctx.FromUin, '不要乱发指令啊喂')
 
 
 bot.run()
