66c10a072ce20d90d6fbd8dadfc155b5d6c3d13b
Centauria <centauriavc@gmail.com>
2020-06-16 21:28:34 +0800

    fix bug of baidu

--- plugins/bot_baidu.py
+++ plugins/bot_baidu.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
 import requests
 from iotbot import GroupMsg, Action
 from util import configuration
@@ -8,7 +10,10 @@ def receive_group_msg(ctx: GroupMsg):
     if ctx.FromUserId != configuration.qq:
         action = Action(configuration.qq)
         if ctx.MsgType == 'TextMsg' and judge_msg(ctx.Content, "百度"):
-            action.send_group_text_msg(ctx.FromGroupId, get_text(ctx.Content[3:]))
+            action.send_group_text_msg(
+                ctx.FromGroupId,
+                os.linesep.join(get_text(ctx.Content[3:]))
+            )
 
 
 # 命令判断
