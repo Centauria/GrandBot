fe0268cedad3754cfd3ec715bb6d1185b9abd222
dell <1730154258@qq.com>
2020-06-17 13:17:49 +0800

    add music research

--- plugins/bot_share.py
+++ plugins/bot_share.py
@@ -13,8 +13,6 @@ def receive_group_msg(ctx: GroupMsg):
             if command[0] == "点歌" and len(command) == 2:
                 content = xml_get_music163(command[1])
                 action.send_group_xml_msg(ctx.FromGroupId, content)
-            else:
-                action.send_group_text_msg(ctx.FromGroupId, "爷发现你输入了非法参数！")
 
 def xml_get_music163(key):
     xml = ""
