3024939cfc6542297f9d43dd001c354f4da3bfe2
dell <1730154258@qq.com>
2020-06-17 17:18:47 +0800

    add music research

--- plugins/bot_test.py
+++ plugins/bot_test.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+import requests
+from iotbot import GroupMsg, Action
+from util import configuration
+
+
+# 分享内容
+def receive_group_msg(ctx: GroupMsg):
+    if ctx.FromUserId != configuration.qq:
+        action = Action(configuration.qq)
+        if ctx.MsgType == 'XmlMsg':
+            print(ctx.Content)
+            action.send_group_xml_msg(ctx.FromGroupId, ctx.Content)
