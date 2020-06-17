b95c74266e8a346ec4e4c0fec0184a8a1dcefb9f
dell <1730154258@qq.com>
2020-06-17 17:31:29 +0800

    add music research

--- plugins/bot_test.py
+++ plugins/bot_test.py
@@ -1,16 +1,1 @@
 # -*- coding: utf-8 -*-
-import requests
-from iotbot import GroupMsg, Action
-from util import configuration
-
-
-# 分享内容
-def receive_group_msg(ctx: GroupMsg):
-    if ctx.FromUserId != configuration.qq:
-        action = Action(configuration.qq)
-        print("TESTTTTT")
-        if ctx.MsgType == 'XmlMsg':
-            content = ctx.Content[11:]
-            content = content[:content.find("[分享]真的爱你\nBEYOND")}
-            print(content)
-            action.send_group_xml_msg(ctx.FromGroupId, content)
