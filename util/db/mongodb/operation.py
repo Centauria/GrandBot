ce6ccf4cd776e432a15bdae409e09476a4853c5c
dell <1730154258@qq.com>
2020-06-18 13:02:30 +0800

    add revoke

--- util/db/mongodb/operation.py
+++ util/db/mongodb/operation.py
@@ -37,18 +37,16 @@ def insert_group_msg(ctx):
             msg_seq=ctx.MsgSeq,
         ))
     elif ctx.MsgType == 'AtMsg':
-        content = json.loads(ctx.Content)
         db.group_msg.insert_one(dict(
             current_qq=ctx.CurrentQQ,
             from_nickname=ctx.FromNickName,
             from_user_id=ctx.FromUserId,
             from_group_name=ctx.FromGroupName,
             from_group_id=ctx.FromGroupId,
-            content=content['Content'],
+            content=ctx.Content,
             msg_time=ctx.MsgTime,
             msg_type=ctx.MsgType,
             msg_seq=ctx.MsgSeq,
-            at_id=content['UserID']
         ))
     else:
         logger.error('Unspecified message type')
