69f2d5d10b21b4971874bb87426cd07e9cd5a6eb
dell <1730154258@qq.com>
2020-06-18 12:46:20 +0800

    add revoke

--- plugins/bot_revoke.py
+++ plugins/bot_revoke.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 import json
 from iotbot import GroupMsg, Action
-import util.db.mongodb.operation as op
+from util.db.mongodb.operation import db
 from util import configuration
 
 
 def receive_events(ctx: dict):
     msg_seq = ctx['CurrentPacket']['Data']['EventData']['MsgSeq']
-    print(f'{msg_seq}')
-    msg_revoke = op.find_group_msg_by_msgseq(msg_seq)
+    msg_revoke = list(db.group_msg.find({"msg_seq":msg_seq}))[0]
+    print(msg_revoke)
     action = Action(configuration.qq)
     if msg_revoke["msg_type"] == 'TextMsg':
-        msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了消息！\n"
-        print(msg_revoke["content"])
+        msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了消息：\n\n"
+        action.send_group_text_msg(msg_revoke["from_group_id"], msg + msg_revoke["content"])
+    if msg_revoke["msg_type"] == 'PicMsg':
+        msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了图片："
         action.send_group_text_msg(msg_revoke["from_group_id"], msg)
+        pic_msg = json.loads(msg_revoke["content"])
+        for pic_content in pic_msg['GroupPic']:
+            action.send_group_pic_msg(msg_revoke["from_group_id"], fileMd5=pic_content['FileMd5'],
+                                      picBase64Buf=pic_content['ForwordBuf'])
+
+
 
