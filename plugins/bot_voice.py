9015ec38e75131753367caeda362ccc65d5cdfb8
dell <1730154258@qq.com>
2020-06-19 20:38:44 +0800

    add voice

--- plugins/bot_voice.py
+++ plugins/bot_voice.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
-import requests
+import pyttsx3
+import time
+import string
+import random
 from iotbot import GroupMsg, Action
 from util import configuration
 
@@ -10,5 +12,44 @@ def receive_group_msg(ctx: GroupMsg):
     if ctx.FromUserId != configuration.qq:
         action = Action(configuration.qq)
         if ctx.MsgType == 'TextMsg':
-            if ctx.Content == "说话":
-                action.send_group_voice_msg(ctx.FromGroupId, "http://grouptalk.c2c.qq.com/?ver=0\u0026rkey=3062020101045b30590201010201010204679c9eaf042439306a336f6f4e45517536386638785574717273666a4930586d6e6a4441313332766b7a02045eec8b88041f0000000866696c6574797065000000013100000005636f64656300000001310400\u0026filetype=1\u0026voice_codec=1")
+            command = ctx.Content.split(' ')
+            if command[0] == "说话 " and len(command) >= 2:
+                msg = command[1]
+                path = '../voice/' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + ''.join(random.sample(string.ascii_letters + string.digits, 8)) +'.mp3'
+                volume = 1.0
+                rate = 100
+                voice = 0
+                for i in range(len(command) - 2):
+                    if command[i+2][:7] == 'volume=':
+                        volume = command[i+2].lstrip('volume=')
+                        try:
+                            volume = float(volume)
+                        except:
+                            action.send_group_text_msg(ctx.FromGroupId, "参数:volume 有误！")
+                    elif command[i+2][:5] == 'rate=':
+                        rate = command[i+2].lstrip('rate=')
+                        try:
+                            rate = int(rate)
+                        except:
+                            action.send_group_text_msg(ctx.FromGroupId, "参数:rate 有误！")
+                    elif command[i+2][:6] == 'voice=':
+                        voice = command[i+2].lstrip('voice=')
+                        try:
+                            voice = int(voice)
+                        except:
+                            action.send_group_text_msg(ctx.FromGroupId, "参数:voice 有误！")
+                save(msg, path, volume, rate, voice)
+                action.send_friend_voice_msg(ctx.FromGroupId, path)
+
+
+
+
+def save(msg, path, volume=1.0, rate=100, voice=0):
+    engine = pyttsx3.init()
+    engine.setProperty('volume', volume)
+    engine.setProperty('rate', rate)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[voice].id)
+    engine.save_to_file(msg, path)
+    engine.runAndWait()
+    engine.stop()
