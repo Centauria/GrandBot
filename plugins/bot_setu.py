afdd71051e3f4824e25d6ee034e4527c4a0383e0
dell <1730154258@qq.com>
2020-06-20 18:32:50 +0800

    update setu

--- plugins/bot_setu.py
+++ plugins/bot_setu.py
@@ -1,20 +1,49 @@
 # -*- coding: utf-8 -*-
 import random
-from iotbot import GroupMsg, Action
+from iotbot import GroupMsg, FriendMsg, Action
 from util import configuration
 
 
-# 输入 “百度 关键字”，回显百科内容
+# setu!
 def receive_group_msg(ctx: GroupMsg):
+    num = {"hentai": 4461, "drawings": 15566}
     if ctx.FromUserId != configuration.qq:
         action = Action(configuration.qq)
         if ctx.MsgType == 'TextMsg':
             command = ctx.Content.split(' ')
             if command[0] == "setu":
-                execute = "hentai"
-                no = random.randint(1, 4611)
-                name = execute + "{:0>5d}".format(no) + '.png'
-                url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
-                action.send_group_pic_msg(ctx.FromGroupId,  picUrl=url)
+
+                if len(command) == 2:
+                    execute = command[1]
+                else:
+                    execute = "hentai"
+
+                for c in num:
+                    if c == execute:
+                        no = random.randint(1, num[c])
+                        name = execute + "{:0>5d}".format(no) + '.png'
+                        url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
+                        action.send_group_pic_msg(ctx.FromGroupId,  picUrl=url)
+
+
+def receive_friend_msg(ctx: FriendMsg):
+    num = {"hentai": 4461, "drawings": 15566}
+    if ctx.FromUin != configuration.qq:
+        action = Action(configuration.qq)
+        if ctx.MsgType == 'TextMsg':
+            command = ctx.Content.split(' ')
+            if command[0] == "setu":
+
+                if len(command) == 2:
+                    execute = command[1]
+                else:
+                    execute = "hentai"
+
+                for c in num:
+                    if c == execute:
+                        no = random.randint(1, num[c])
+                        name = execute + "{:0>5d}".format(no) + '.png'
+                        url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
+                        action.send_group_pic_msg(ctx.FromGroupId, picUrl=url)
 
 
