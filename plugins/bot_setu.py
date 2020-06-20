56635dd8f41d2347bd19b31bbcfca0aa779be758
dell <1730154258@qq.com>
2020-06-20 17:51:39 +0800

    add setu

--- plugins/bot_setu.py
+++ plugins/bot_setu.py
@@ -6,14 +6,15 @@ from util import configuration
 
 # 输入 “百度 关键字”，回显百科内容
 def receive_group_msg(ctx: GroupMsg):
-    action = Action(configuration.qq)
-    if ctx.MsgType == 'TextMsg':
-        command = ctx.Content.split(' ')
-        if command[0] == "setu":
-            execute = "hentai"
-            no = random.randint(1, 4611)
-            name = execute + "{:0>5d}".format(no) + '.png'
-            url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
-            action.send_group_pic_msg(ctx.FromGroupId,  picUrl=url)
+    if ctx.FromUserId != configuration.qq:
+        action = Action(configuration.qq)
+        if ctx.MsgType == 'TextMsg':
+            command = ctx.Content.split(' ')
+            if command[0] == "setu":
+                execute = "hentai"
+                no = random.randint(1, 4611)
+                name = execute + "{:0>5d}".format(no) + '.png'
+                url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
+                action.send_group_pic_msg(ctx.FromGroupId,  picUrl=url)
 
 
