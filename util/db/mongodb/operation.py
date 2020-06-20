34708bfdd0acbd34c06ca54e7954cb467134f698
Centauria <centauriavc@gmail.com>
2020-06-21 03:09:42 +0800

    some corrections

--- util/db/mongodb/operation.py
+++ util/db/mongodb/operation.py
@@ -2,13 +2,18 @@
 import json
 import logging
 from pymongo.results import InsertOneResult
+from iotbot import GroupMsg, FriendMsg
 
 from .connection import db
 
 logger = logging.Logger('MongoDB')
 
 
-def insert_group_msg(ctx):
+def find_group_msg_by_msg_seq(msg_seq):
+    return db.group_msg.find({"msg_seq": msg_seq}).sort({"uploadTime": -1})
+
+
+def insert_group_msg(ctx: GroupMsg):
     if ctx.MsgType == 'TextMsg':
         content = ctx.Content
         db.group_msg.insert_one(dict(
@@ -52,47 +57,34 @@ def insert_group_msg(ctx):
         logger.error('Unspecified message type')
 
 
-def find_group_msg_by_msgseq(msg_seq):
-    return db.group_nsg.find({"msg_seq": msg_seq}).sort({"uploadTime": -1})
-
-
-def insert_friend_msg(ctx):
+def insert_friend_msg(ctx: FriendMsg):
     if ctx.MsgType == 'TextMsg':
         content = ctx.Content
-        db.group_msg.insert_one(dict(
+        db.friend_msg.insert_one(dict(
             current_qq=ctx.CurrentQQ,
-            from_nickname=ctx.FromNickName,
-            from_user_id=ctx.FromUserId,
-            from_group_name=ctx.FromGroupName,
-            from_group_id=ctx.FromGroupId,
+            from_user_id=ctx.FromUin,
             content=content,
-            msg_time=ctx.MsgTime,
+            msg_time='???',
             msg_type=ctx.MsgType,
             msg_seq=ctx.MsgSeq,
         ))
     elif ctx.MsgType == 'PicMsg':
         content = json.loads(ctx.Content)
         ret: InsertOneResult = db.img.insert_one(content)
-        db.group_msg.insert_one(dict(
+        db.friend_msg.insert_one(dict(
             current_qq=ctx.CurrentQQ,
-            from_nickname=ctx.FromNickName,
-            from_user_id=ctx.FromUserId,
-            from_group_name=ctx.FromGroupName,
-            from_group_id=ctx.FromGroupId,
+            from_user_id=ctx.FromUin,
             content=content,
-            msg_time=ctx.MsgTime,
+            msg_time='???',
             msg_type=ctx.MsgType,
             msg_seq=ctx.MsgSeq,
         ))
     elif ctx.MsgType == 'AtMsg':
-        db.group_msg.insert_one(dict(
+        db.friend_msg.insert_one(dict(
             current_qq=ctx.CurrentQQ,
-            from_nickname=ctx.FromNickName,
-            from_user_id=ctx.FromUserId,
-            from_group_name=ctx.FromGroupName,
-            from_group_id=ctx.FromGroupId,
+            from_user_id=ctx.FromUin,
             content=ctx.Content,
-            msg_time=ctx.MsgTime,
+            msg_time='???',
             msg_type=ctx.MsgType,
             msg_seq=ctx.MsgSeq,
         ))
