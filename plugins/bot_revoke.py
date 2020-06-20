1198af3694d235ed086ce21bc85787d6a9b853f3
dell <1730154258@qq.com>
2020-06-20 17:28:37 +0800

    update remoke

--- plugins/bot_revoke.py
+++ plugins/bot_revoke.py
@@ -6,21 +6,22 @@ from util import configuration
 
 
 def receive_events(ctx: dict):
-    msg_seq = ctx['CurrentPacket']['Data']['EventData']['MsgSeq']
-    msg_revoke = list(db.group_msg.find({"msg_seq":msg_seq}))[0]
-    print(msg_revoke)
-    action = Action(configuration.qq)
-    if msg_revoke["msg_type"] == 'TextMsg':
-        msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了消息：\n\n"
-        action.send_group_text_msg(msg_revoke["from_group_id"], msg + msg_revoke["content"])
-    if msg_revoke["msg_type"] == 'PicMsg':
-        msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了图片："
-        action.send_group_text_msg(msg_revoke["from_group_id"], msg)
-        pic_msg = msg_revoke["content"]
-        print(pic_msg)
-        for pic_content in pic_msg['GroupPic']:
-            action.send_group_pic_msg(msg_revoke["from_group_id"], fileMd5=pic_content['FileMd5'],
-                                      picBase64Buf=pic_content['ForwordBuf'])
+    if ctx['CurrentPacket']['Data']['EventData']['UserID'] != configuration.qq:
+        action = Action(configuration.qq)
+        msg_seq = ctx['CurrentPacket']['Data']['EventData']['MsgSeq']
+        msg_revoke = list(db.group_msg.find({"msg_seq":msg_seq}))[0]
+        print(msg_revoke)
+        if msg_revoke["msg_type"] == 'TextMsg':
+            msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了消息：\n\n"
+            action.send_group_text_msg(msg_revoke["from_group_id"], msg + msg_revoke["content"])
+        if msg_revoke["msg_type"] == 'PicMsg':
+            msg = "爷发现 " + msg_revoke["from_nickname"] + " 撤回了图片："
+            action.send_group_text_msg(msg_revoke["from_group_id"], msg)
+            pic_msg = msg_revoke["content"]
+            print(pic_msg)
+            for pic_content in pic_msg['GroupPic']:
+                action.send_group_pic_msg(msg_revoke["from_group_id"], fileMd5=pic_content['FileMd5'],
+                                          picBase64Buf=pic_content['ForwordBuf'])
 
 
 
