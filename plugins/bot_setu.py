51f4d35bf22842fbfb172f38679f9471b06d8745
dell <1730154258@qq.com>
2020-06-20 17:08:36 +0800

    add setu

--- plugins/bot_setu.py
+++ plugins/bot_setu.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import random
-from iotbot import FriendMsg, Action
+from iotbot import GroupMsg, Action
 from util import configuration
 
 
 # 输入 “百度 关键字”，回显百科内容
-def receive_friend_msg(ctx: FriendMsg):
+def receive_friend_msg(ctx: GroupMsg):
     action = Action(configuration.qq)
     if ctx.MsgType == 'TextMsg':
         command = ctx.Content.split(' ')
@@ -14,6 +14,6 @@ def receive_friend_msg(ctx: FriendMsg):
             no = random.randint(1, 4611)
             name = execute + "{:0>5d}".format(no) + '.png'
             url = "http://jinfans.top/others/iotbot/bot_setu_image/hentai/image/" + name
-            action.send_friend_pic_msg(ctx.FromUin, url)
+            print(action.send_friend_pic_msg(ctx.FromGroupId, picUrl=url))
 
 
--- util/db/mongodb/operation.py
+++ util/db/mongodb/operation.py
@@ -54,3 +54,47 @@ def insert_group_msg(ctx):
 
 def find_group_msg_by_msgseq(msg_seq):
     return db.group_nsg.find({"msg_seq":msg_seq}).sort({"uploadTime": -1})
+
+
+def insert_friend_msg(ctx):
+    if ctx.MsgType == 'TextMsg':
+        content = ctx.Content
+        db.group_msg.insert_one(dict(
+            current_qq=ctx.CurrentQQ,
+            from_nickname=ctx.FromNickName,
+            from_user_id=ctx.FromUserId,
+            from_group_name=ctx.FromGroupName,
+            from_group_id=ctx.FromGroupId,
+            content=content,
+            msg_time=ctx.MsgTime,
+            msg_type=ctx.MsgType,
+            msg_seq=ctx.MsgSeq,
+        ))
+    elif ctx.MsgType == 'PicMsg':
+        content = json.loads(ctx.Content)
+        ret: InsertOneResult = db.img.insert_one(content)
+        db.group_msg.insert_one(dict(
+            current_qq=ctx.CurrentQQ,
+            from_nickname=ctx.FromNickName,
+            from_user_id=ctx.FromUserId,
+            from_group_name=ctx.FromGroupName,
+            from_group_id=ctx.FromGroupId,
+            content=content,
+            msg_time=ctx.MsgTime,
+            msg_type=ctx.MsgType,
+            msg_seq=ctx.MsgSeq,
+        ))
+    elif ctx.MsgType == 'AtMsg':
+        db.group_msg.insert_one(dict(
+            current_qq=ctx.CurrentQQ,
+            from_nickname=ctx.FromNickName,
+            from_user_id=ctx.FromUserId,
+            from_group_name=ctx.FromGroupName,
+            from_group_id=ctx.FromGroupId,
+            content=ctx.Content,
+            msg_time=ctx.MsgTime,
+            msg_type=ctx.MsgType,
+            msg_seq=ctx.MsgSeq,
+        ))
+    else:
+        logger.error('Unspecified message type')
