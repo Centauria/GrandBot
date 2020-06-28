5b1e472ea8fcbcd62d8dd22b2501d203fbd30169
dell <1730154258@qq.com>
2020-06-28 19:51:00 +0800

    update latex generator

--- plugins/bot_latex.py
+++ plugins/bot_latex.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from iotbot import GroupMsg, Action
 from util import configuration
-
+import re
 from util.network.request import post
 
 
@@ -13,14 +13,24 @@ def receive_group_msg(ctx: GroupMsg):
 
             command = ctx.Content.split(' ', 1)
             if command[0] == "latex" and len(command) > 1:
-                latex_content = command[1].replace("\\", "\\\\")
+                latex_content = command[1]
                 url = 'https://quicklatex.com/latex3.f'
-                data = {"formula": latex_content, "fcolor": "000000", "fsize": "17px", "mode": "0", "out": "1", "remhost": "quicklatex.com"}
-                response = post(url, data)
 
-                content = response.text.split("\r\n")[1]
+                data = {"formula": transfer_spacing(latex_content), "fcolor": "000000", "fsize": "100px", "mode": "0", "out": "1", "remhost": "quicklatex.com"}
+                response = post(url, data)
+                content = response.text.split("\r\n")
                 if content[0] == "0":
                     pic_url = content[1].split(' ')[0]
-                    action.send_group_pic_msg(ctx.FromGroupId, picUrl=pic_url)
+                    print(pic_url)
+                    action.send_group_pic_msg(ctx.FromGroupId, picUrl=pic_url, timeout=10)
                 else:
                     action.send_group_text_msg(ctx.FromGroupId, "无法识别公式！")
+
+
+#将空格转义
+def transfer_spacing(latex_content):
+    c_list = [i.start() for i in re.finditer(' ', latex_content)]
+    new_content = list(latex_content)
+    for i in range(len(c_list)):
+        new_content.insert(c_list[i] + i, '\\')
+    return "".join(new_content)
