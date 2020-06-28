6b7bde68038c6cab3456b617983e6c5ea7f27f48
dell <1730154258@qq.com>
2020-06-28 19:53:08 +0800

    update latex generator

--- plugins/bot_latex.py
+++ plugins/bot_latex.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from iotbot import GroupMsg, Action
+from iotbot import GroupMsg, FriendMsg, Action
 from util import configuration
 import re
 from util.network.request import post
@@ -16,16 +16,35 @@ def receive_group_msg(ctx: GroupMsg):
                 latex_content = command[1]
                 url = 'https://quicklatex.com/latex3.f'
 
-                data = {"formula": transfer_spacing(latex_content), "fcolor": "000000", "fsize": "100px", "mode": "0", "out": "1", "remhost": "quicklatex.com"}
+                data = {"formula": transfer_spacing(latex_content), "fcolor": "000000", "fsize": "70px", "mode": "0", "out": "1", "remhost": "quicklatex.com"}
                 response = post(url, data)
                 content = response.text.split("\r\n")
                 if content[0] == "0":
                     pic_url = content[1].split(' ')[0]
                     print(pic_url)
-                    action.send_group_pic_msg(ctx.FromGroupId, picUrl=pic_url, timeout=10)
+                    action.send_group_pic_msg(ctx.FromGroupId, picUrl=pic_url, timeout=20)
                 else:
                     action.send_group_text_msg(ctx.FromGroupId, "无法识别公式！")
 
+
+def receive_friend_msg(ctx: FriendMsg):
+    action = Action(configuration.qq)
+    if ctx.MsgType == 'TextMsg':
+
+        command = ctx.Content.split(' ', 1)
+        if command[0] == "latex" and len(command) > 1:
+            latex_content = command[1]
+            url = 'https://quicklatex.com/latex3.f'
+
+            data = {"formula": transfer_spacing(latex_content), "fcolor": "000000", "fsize": "70px", "mode": "0", "out": "1", "remhost": "quicklatex.com"}
+            response = post(url, data)
+            content = response.text.split("\r\n")
+            if content[0] == "0":
+                pic_url = content[1].split(' ')[0]
+                print(pic_url)
+                action.send_friend_pic_msg(ctx.FromUin, picUrl=pic_url, timeout=20)
+            else:
+                action.send_friend_text_msg(ctx.FromUin, "无法识别公式！")
 
 #将空格转义
 def transfer_spacing(latex_content):
