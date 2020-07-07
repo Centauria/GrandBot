b2292e0aa635b6860b398425d84b76b9585b8c84
dell <1730154258@qq.com>
2020-07-07 22:19:51 +0800

    fix echo

--- plugins/bot_echo.py
+++ plugins/bot_echo.py
@@ -12,4 +12,4 @@ def receive_group_msg(ctx: GroupMsg):
 
 			if ctx.Content[:5] == "echo ":
 				command_test = ctx.Content.lstrip("echo ")
-				action.send_group_text_msg(ctx.FromGroupId, atUser=ctx.FromUserId, content=command_test)
+				action.send_group_text_msg(ctx.FromGroupId, content=command_test)
