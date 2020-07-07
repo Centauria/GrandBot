8914479420715b37ed30a9b4c48873b4bd44f110
dell <1730154258@qq.com>
2020-07-07 22:16:46 +0800

    add echo

---
+++ plugins/bot_echo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import time
+from iotbot import GroupMsg, Action
+from util import configuration
+
+
+# echo功能
+def receive_group_msg(ctx: GroupMsg):
+	if ctx.FromUserId != configuration.qq:
+		action = Action(configuration.qq)
+		if ctx.MsgType == 'TextMsg':
+
+			if ctx.Content[:5] == "echo ":
+				command_test = ctx.Content.lstrip("echo ")
+				action.send_group_text_msg(ctx.FromGroupId, atUser=ctx.FromUserId, content=command_test)
